eb482ad31c68c5ae1f2e032ae1adff79297d05b4
Charis <haris.gjo@gmail.com>
2020-12-02 12:04:55 +0200

    changes in index, might make it responsive in the future

--- index.php
+++ index.php
@@ -20,49 +20,17 @@ if(!isset($_SESSION['loggedin'])){ //if login in session is not set
     <body class="sb-nav-fixed">
         <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
             <a class="navbar-brand" href="index.php">CG-Bugtracker</a>
-            <button class="btn btn-link btn-sm order-1 order-lg-0" id="sidebarToggle" href="#"><i class="fas fa-bars"></i></button>
-            <!-- Navbar Search-->
-            <form class="d-none d-md-inline-block form-inline ml-auto mr-0 mr-md-3 my-2 my-md-0">
-                <div class="input-group">
-                    <input class="form-control" type="text" placeholder="Search for..." aria-label="Search" aria-describedby="basic-addon2" />
-                    <div class="input-group-append">
-                        <button class="btn btn-primary" type="button"><i class="fas fa-search"></i></button>
-                    </div>
-                </div>
-            </form>
             <!-- Navbar-->
-            <ul class="navbar-nav ml-auto ml-md-0">
+            <ul class="navbar-nav d-md-inline-block ml-auto">
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" id="userDropdown" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="fas fa-user fa-fw"></i></a>
                     <div class="dropdown-menu dropdown-menu-right" aria-labelledby="userDropdown">
-                        <a class="dropdown-item" href="#">Settings</a>
-                        <a class="dropdown-item" href="#">Activity Log</a>
                         <div class="dropdown-divider"></div>
+                        <a href="account_info.php" class="dropdown-item">Account</a>
                         <a class="dropdown-item" href="logout.php">Logout</a>
-                    </div>
-                </li>
-            </ul>
-        </nav>
-        <div id="layoutSidenav">
-            <div id="layoutSidenav_nav">
-                <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
-                    <div class="sb-sidenav-menu">
-                        <div class="nav">
-                            <a class="nav-link" href="index.php">
-                                <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
-                                User Projects:
-                            </a>
-                            <ul>
-                                <li> <a href="add_project.php">Add a project</a> </li>
-                            </ul>
-
-                            
-                        
-                        </div>
-                    </div>
-                    <div class="sb-sidenav-footer">
-                        <div class="small">Logged in as:</div>
-                        Username:
+                        <div class="dropdown-divider"></div>
+                        <div class="small dropdown-item disabled">Logged in as:</div>
+                        <div class="dropdown-item disabled">
                         <?php
                             require_once "config.php";
                             $id = $_SESSION["id"];
@@ -75,18 +43,20 @@ if(!isset($_SESSION['loggedin'])){ //if login in session is not set
                                 }
                             }
                             echo $name, " ", $surname;
-                        ?>
-                        <br>
-                        Email:
-                        <?php
+                            ?>
+                            <br>
+                            <?php
                             echo $_SESSION["email"];
                         ?>
+                        </div>
                     </div>
-                </nav>
-            </div>
-            <div id="layoutSidenav_content">
-                <main>
+                </li>
+            </ul>
+        </nav>
+        <main>
                     <div class="container-fluid">
+                        <br>
+                        <br>
                         <h1 class="mt-4">Dashboard</h1>
                         <ol class="breadcrumb mb-4">
                             <li class="breadcrumb-item active">
@@ -137,7 +107,7 @@ if(!isset($_SESSION['loggedin'])){ //if login in session is not set
                 
                 </main>
 
-                <footer class="py-4 bg-light mt-auto">
+                <!-- <footer class="py-4 bg-light mt-auto">
                     <div class="container-fluid">
                         <div class="d-flex align-items-center justify-content-between small">
                             <div class="text-muted">Copyright &copy; Your Website 2020</div>
@@ -148,9 +118,7 @@ if(!isset($_SESSION['loggedin'])){ //if login in session is not set
                             </div>
                         </div>
                     </div>
-                </footer>
-            </div>
-        </div>
+                </footer> -->
         <script src="https://code.jquery.com/jquery-3.5.1.min.js" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
         <script src="js/scripts.js"></script>
